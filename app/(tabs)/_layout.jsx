1bdbb57dbb49c32d6032394ee1bcfdce8750ba6e
Sonkarrushikesh22 <98597295+Sonkarrushikesh22@users.noreply.github.com>
2024-10-28 20:33:09 +0530

    tab-bar implemented

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,15 +1,45 @@
 import { View, Text } from 'react-native'
 import { Redirect, Stack } from "expo-router";
-
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Tabs } from 'expo-router';
 import React from 'react'
 
 const TabsLayout = () => {
   return (
-<Stack screenOptions={{ headerShown: false }} >
-      <Stack.Screen name="home" />
-      <Stack.Screen name="explore" />
-    </Stack>
-  )
+    <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }} >
+      <Tabs.Screen
+        name="home"
+        options={{
+          headerShown: false ,
+          title: 'Home',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: 'Explore',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
+        }}
+      />
+         <Tabs.Screen
+        name="saved"
+        options={{
+          title: 'Saved',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
+        }}
+      />
+         <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
+        }}
+      />
+    </Tabs>
+    
+    
+  );
 }
 
 export default TabsLayout
