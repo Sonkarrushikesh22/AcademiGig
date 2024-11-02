d103acfbd3f66522c87de60ac513785aebb51d32
Sonkarrushikesh22 <98597295+Sonkarrushikesh22@users.noreply.github.com>
2024-11-02 20:12:07 +0530

    chat and notifications navigations added

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,9 +1,12 @@
-import { View, Text, ScrollView, FlatList } from "react-native";
+import { View, Text, ScrollView, FlatList, TouchableOpacity } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import SimpleLineIcons from "@expo/vector-icons/SimpleLineIcons";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import  SearchInput  from "../../components/Search/SearchInput";
+// import Chat from "../../components/Headertabs/chat"
+// import Notification from "../../components/Headertabs/notifications";
+import { useRouter, Link } from 'expo-router';
 
 const Styles = {
   Headercontainer: {
@@ -30,6 +33,8 @@ const Styles = {
 };
 
 const Home = () => {
+  const router = useRouter();
+
   const handleSearch = (text) => {
     console.log("Search:", text);
   };
@@ -43,8 +48,16 @@ const Home = () => {
         <View style={Styles.Headercontainer}>
           <Text style={Styles.logo}>AcademiGig</Text>
           <View style={Styles.icons}>
+            <TouchableOpacity
+             onPress={() => router.push("/HeaderTabs/notifications")}
+            >
             <SimpleLineIcons name="bell" size={24} color="black" paddingHorizontal={5} />
+            </TouchableOpacity>
+            <TouchableOpacity
+            onPress={() => router.push("/HeaderTabs/chat")}
+            >
             <Ionicons name="chatbubbles-outline" size={24} color="black" paddingHorizontal={5}/>
+            </TouchableOpacity>
           </View>
         </View>
         
--- app/HeaderTabs/notifications.jsx
+++ app/HeaderTabs/notifications.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 
 const notifications = () => {
   return (
-    <View>
+   
+    <View >
       <Text>notifications</Text>
     </View>
   )
