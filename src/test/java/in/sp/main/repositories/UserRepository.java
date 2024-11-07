a1d36872b678ad87b2974fe4d8cbf964cfd5868a
Sonkarrushikesh22 <98597295+Sonkarrushikesh22@users.noreply.github.com>
2024-11-07 23:13:27 +0530

    Commit all local changes

--- src/test/java/in/sp/main/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package in.sp.main.repositories;
-
-import org.apache.catalina.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-}
